3509c22b1355a87134cdda60f4ff782e09893301
Wylarel <timjemine@gmail.com>
2022-10-18 17:04:46 +0200

    Introduce functions

--- functions.py
+++ functions.py
@@ -1,13 +1,16 @@
-test = {
-    "length": 8,  # Length in beats
-    "steps": [
-        [0, (0, 0, 0)],  # Premier temps
-        [1, (1, 0, 0)],
-        [1, (1, 1, 0)],
-        [2, (0, 0, 0)],
-        [4, (1, 0, 0)],  # Cinquième temps
-        [6, (.5, 1, 1)],
-        [6, (0, 0, 0)],
-        [7, (1, 0, .5)],  # Huitième temps
-    ]
-}
+test = [
+    [0, (0, 0, 0)],  # Premier temps
+    [1, (1, 0, 0)],
+    [1, (1, 1, 0)],
+    [2, (0, 0, 0)],
+    [4, (1, 0, 0)],  # Cinquième temps
+    [6, (.5, 1, 1)],
+    [6, (0, 0, 0)],
+    [7, (1, 0, .2)],  # Huitième temps
+    [8, (1, 1, 1)],   # Fin de 8ème temps
+]
+
+long_fade_from_black = [
+    [0, (0, 0, 0)],
+    [16, (1, 1, 1)],
+]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Traffic light for Kraktakara
 # 120mA / light = 360mA
 
-import time
+from time import perf_counter as timer, sleep
 import pigpio
 
 from cie1931 import cie
@@ -13,45 +13,62 @@ RedPin = 17
 YellowPin = 22
 GreenPin = 24
 
+steps = functions.long_fade_from_black
+
+bpm = 120
+bps = bpm / 60
+
 
 def setled(pin, brightness):
     linearised = cie[round(brightness * 255)]  # Linearise led intensity (In 0<255 Out 0<1023)
     pi.set_PWM_dutycycle(pin, linearised / 4)  # Set GPIO output PWM (0<255)
 
 
-def loop():
-    beat_length = 0.5  # 120BPM Equivalent
-    start_time = time.time()
-    time_since_start = .0
-    last_beat_time = .0
-    time_since_beat = .0
-    beats_since_start = 0
-    position_in_beat = .0
+def lerp(decimal_beat: float, prev_step_idx: int, next_step_idx: int):
+    prev_step = steps[prev_step_idx]
+    if next_step_idx < len(steps):
+        next_step = steps[next_step_idx]
+        prev_beat = prev_step[0]
+        next_beat = next_step[0]
+    else:
+        next_step = steps[0]
+        prev_beat = prev_step[0]
+        next_beat = len(steps)+1
 
-    func = functions.test  # Fonction test
-    func_steps = func["steps"]
-    func_length = func["length"]
+    prev_step = prev_step[1]
+    next_step = next_step[1]
 
-    while True:
-        current_time = time.time()
-        time_since_start = current_time - start_time
-        time_since_beat = time_since_start - last_beat_time
-        position_in_beat = time_since_beat / beat_length
-        if time_since_beat > beat_length:
-            last_beat_time = time_since_start
-            time_since_beat = .0
-            beats_since_start += 1
-            position_in_beat = .0
-        # print(time_since_start, beats_since_start+position_in_beat)
+    assert prev_beat <= decimal_beat <= next_beat, f"{prev_beat}, {decimal_beat}, {next_beat}"
+    progress = (decimal_beat - prev_beat) / (next_beat - prev_beat)
 
-        beat = beats_since_start % func_length
-
-        # Implémenter l'interpolation linéaire ici
-
-        setled(RedPin, 0)
-        setled(YellowPin, 0)
-        setled(GreenPin, 0)
+    return tuple(
+        prev + (next - prev) * progress
+        for prev, next in zip(prev_step, next_step)
+    )
 
 
-if __name__ == '__main__':
-    loop()
+start_time = timer()
+while True:
+    rel_time = timer() - start_time
+    length = steps[len(steps)-1][0]
+    decimal_beat = (rel_time * bps)%length
+
+    # Find the previous and the current step
+    next_step_idx = 0
+    while next_step_idx < len(steps) and steps[next_step_idx][0] < decimal_beat:
+        next_step_idx += 1
+    prev_step_idx = max(0, next_step_idx - 1)
+
+    if next_step_idx >= len(steps):
+        prev_step_idx = 0
+        next_step_idx = 0
+    pos = lerp(decimal_beat, prev_step_idx, next_step_idx)
+
+    print("At time {:.03f}, beat {:.03f}".format(rel_time, decimal_beat), end="")
+    print(", pos: {:.02f}, {:.02f}, {:.02f}".format(*pos))
+
+    setled(RedPin, pos[0])
+    setled(YellowPin, pos[1])
+    setled(GreenPin, pos[2])
+
+    sleep(0.01)
