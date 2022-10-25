cebe6f258e6cd379bf5e3a21a0f2ce46cce41764
Wylarel <timjemine@gmail.com>
2022-10-25 14:06:17 +0200

    Introduce web interface and sound reactive mode

--- functions.py
+++ functions.py
@@ -1,16 +1,80 @@
-test = [
-    [0, (0, 0, 0)],  # Premier temps
-    [1, (1, 0, 0)],
-    [1, (1, 1, 0)],
-    [2, (0, 0, 0)],
-    [4, (1, 0, 0)],  # Cinquième temps
-    [6, (.5, 1, 1)],
-    [6, (0, 0, 0)],
-    [7, (1, 0, .2)],  # Huitième temps
-    [8, (1, 1, 1)],   # Fin de 8ème temps
-]
+# Traffic light for Kraktakara
+# 120mA / light = 360mA
 
-long_fade_from_black = [
-    [0, (0, 0, 0)],
-    [16, (1, 1, 1)],
-]
+from time import perf_counter as timer, sleep
+import pigpio
+
+from cie1931 import cie
+import functions_src
+
+pi = pigpio.pi()
+
+RedPin = 17
+YellowPin = 22
+GreenPin = 24
+
+steps = functions_src.ping_pong_fade
+
+bpm = 120
+bpm_multiplier = 4
+bps = bpm / 60 * bpm_multiplier
+
+
+def setled(pin, brightness):
+    linearised = cie[round(brightness * 255)]  # Linearise led intensity (In 0<255 Out 0<1023)
+    pi.set_PWM_dutycycle(pin, linearised / 4)  # Set GPIO output PWM (0<255)
+
+
+def lerp(decimal_beat: float, prev_step_idx: int, next_step_idx: int):
+    prev_step = steps[prev_step_idx]
+    if next_step_idx < len(steps):
+        next_step = steps[next_step_idx]
+        prev_beat = prev_step[0]
+        next_beat = next_step[0]
+    else:
+        next_step = steps[0]
+        prev_beat = prev_step[0]
+        next_beat = len(steps)+1
+
+    prev_step = prev_step[1]
+    next_step = next_step[1]
+
+    assert prev_beat <= decimal_beat <= next_beat, f"{prev_beat}, {decimal_beat}, {next_beat}"
+    progress = (decimal_beat - prev_beat) / (next_beat - prev_beat)
+
+    return tuple(
+        prev + (next - prev) * progress
+        for prev, next in zip(prev_step, next_step)
+    )
+
+
+def loop():
+    start_time = timer()
+    while True:
+        rel_time = timer() - start_time
+        length = steps[len(steps)-1][0]
+        decimal_beat = (rel_time * bps)%length
+
+        # Find the previous and the current step
+        next_step_idx = 0
+        while next_step_idx < len(steps) and steps[next_step_idx][0] < decimal_beat:
+            next_step_idx += 1
+        prev_step_idx = max(0, next_step_idx - 1)
+
+        if next_step_idx >= len(steps):
+            prev_step_idx = 0
+            next_step_idx = 0
+        pos = lerp(decimal_beat, prev_step_idx, next_step_idx)
+
+        # print("At time {:.03f}, beat {:.03f}".format(rel_time, decimal_beat), end="")
+        # print(", pos: {:.02f}, {:.02f}, {:.02f}".format(*pos))
+
+        setled(RedPin, pos[0])
+        setled(YellowPin, pos[1])
+        setled(GreenPin, pos[2])
+
+        sleep(0.01)
+
+
+if __name__ == '__main__':
+    loop()
--- main.py
+++ main.py
@@ -1,74 +1,52 @@
-# Traffic light for Kraktakara
-# 120mA / light = 360mA
+from flask import Flask, render_template, request
 
-from time import perf_counter as timer, sleep
-import pigpio
+app = Flask(__name__)
 
-from cie1931 import cie
-import functions
-
-pi = pigpio.pi()
-
-RedPin = 17
-YellowPin = 22
-GreenPin = 24
-
-steps = functions.long_fade_from_black
-
-bpm = 120
-bps = bpm / 60
+modes = ['Startup', 'Static', 'Loop', 'Automatic', 'Sound']
+funcs = ['Slow Fade', 'Fast Fade', 'Strobe']
 
 
-def setled(pin, brightness):
-    linearised = cie[round(brightness * 255)]  # Linearise led intensity (In 0<255 Out 0<1023)
-    pi.set_PWM_dutycycle(pin, linearised / 4)  # Set GPIO output PWM (0<255)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    red = 0
+    yellow = 0
+    green = 0
+    strobe = 0
+    sensivity = 0
+
+    if request.method == 'POST':
+        print(request.form)
+        if request.form.get('mode'):
+            selected_mode = request.form.get('mode')
+            modes.remove(selected_mode)
+            modes.insert(0, selected_mode)
+            if selected_mode == "Startup":
+                pass
+            elif selected_mode == "Static":
+                red = request.form.get('red')
+                yellow = request.form.get('yellow')
+                green = request.form.get('green')
+            elif selected_mode == "Loop":
+                selected_func = request.form.get('func')
+                if not selected_func:
+                    selected_func = funcs[0]
+                funcs.remove(selected_func)
+                funcs.insert(0, selected_func)
+                strobe = request.form.get('strobe')
+            elif selected_mode == "Automatic":
+                pass
+            elif selected_mode == "Sound":
+                sensivity = request.form.get('sensivity')
+            return render_template('index.html', modes=modes, funcs=funcs, red=red, yellow=yellow, green=green, strobe=strobe, sensivity=sensivity)
+        else:
+            pass
+        return render_template('index.html', modes=modes, funcs=funcs, red=red, yellow=yellow, green=green, strobe=strobe, sensivity=sensivity)
+    elif request.method == 'GET':
+        return render_template('index.html', modes=modes, funcs=funcs, red=red, yellow=yellow, green=green, strobe=strobe, sensivity=sensivity)
+
+    return render_template('index.html', modes=modes, funcs=funcs, red=red, yellow=yellow, green=green, strobe=strobe,
+                           sensivity=sensivity)
 
 
-def lerp(decimal_beat: float, prev_step_idx: int, next_step_idx: int):
-    prev_step = steps[prev_step_idx]
-    if next_step_idx < len(steps):
-        next_step = steps[next_step_idx]
-        prev_beat = prev_step[0]
-        next_beat = next_step[0]
-    else:
-        next_step = steps[0]
-        prev_beat = prev_step[0]
-        next_beat = len(steps)+1
-
-    prev_step = prev_step[1]
-    next_step = next_step[1]
-
-    assert prev_beat <= decimal_beat <= next_beat, f"{prev_beat}, {decimal_beat}, {next_beat}"
-    progress = (decimal_beat - prev_beat) / (next_beat - prev_beat)
-
-    return tuple(
-        prev + (next - prev) * progress
-        for prev, next in zip(prev_step, next_step)
-    )
-
-
-start_time = timer()
-while True:
-    rel_time = timer() - start_time
-    length = steps[len(steps)-1][0]
-    decimal_beat = (rel_time * bps)%length
-
-    # Find the previous and the current step
-    next_step_idx = 0
-    while next_step_idx < len(steps) and steps[next_step_idx][0] < decimal_beat:
-        next_step_idx += 1
-    prev_step_idx = max(0, next_step_idx - 1)
-
-    if next_step_idx >= len(steps):
-        prev_step_idx = 0
-        next_step_idx = 0
-    pos = lerp(decimal_beat, prev_step_idx, next_step_idx)
-
-    print("At time {:.03f}, beat {:.03f}".format(rel_time, decimal_beat), end="")
-    print(", pos: {:.02f}, {:.02f}, {:.02f}".format(*pos))
-
-    setled(RedPin, pos[0])
-    setled(YellowPin, pos[1])
-    setled(GreenPin, pos[2])
-
-    sleep(0.01)
+if __name__ == '__main__':
+    app.run(port=80)
